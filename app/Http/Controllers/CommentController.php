1a60c3a5decf74cbb44096d8e6046e2e34f63c65
Fabian Hoogendoorn <fbmhoogendoorn@gmail.com>
2020-05-04 12:03:19 +0200

    2.0

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,10 +1,57 @@
 <?php
 
+
+
 namespace App\Http\Controllers;
+
+
 
 use Illuminate\Http\Request;
 
+
+
+use Auth;
+
+use App\Comment;
+
+use App\Post;
+
+
+
 class CommentController extends Controller
+
 {
-    //
+
+    public function store($postid){
+
+        $post = Post::findOrFail($postid);
+
+
+
+        $comment = new Comment();
+
+        $comment->content = request('txtComment');
+
+        $comment->user_id = Auth::user()->id;
+
+        $comment->post_id = $post->id;
+
+        $comment->save();
+
+        return redirect()->route('post.show', $post->slug);
+
+    }
+
+
+
+    public function destroy($commentid){
+
+        $comment = Comment::findOrFail($commentid);
+
+        $comment->delete();
+
+        return redirect()->route('post.show', $comment->post->slug);
+
+    }
+
 }
--- resources/views/post/single.blade.php
+++ resources/views/post/single.blade.php
@@ -44,12 +44,94 @@
 
                     @endauth
 
+                    <div class="comments">
+
+                        @foreach($post->comments as $comment)
+
+                            <div class="comment">
+
+                                <div class="comment-image">
+
+                                    <img src="/images/user.png">
+
+                                </div>
+
+                                <div class="comment-details">
+
+                                    <i class="comment-info">Auteur opmerking: {{$comment->author->name}} at {{$comment->created_at}}</i>
+
+                                    <p class="comment-content">{{$comment->content}}</p>
+
+                                    @auth()
+
+                                        <button class="btn btn-danger comment-delete" onclick="event.preventDefault(); document.getElementById('delete-comment-form').submit();">
+
+                                            Verwijder je opmerking
+
+                                        </button>
+
+                                        <form id="delete-comment-form" action="/comment/{{$comment->id}}" method="post" style="display: none;">
+
+                                            @csrf
+
+                                            @method('delete')
+
+                                        </form>
+
+                                    @endauth
+
+                                </div>
+
+                            </div>
+
+                        @endforeach
+
+                    </div>
+
+                </div>
+
+                <hr>
+
+                <div class="single-post-comment">
+
+                    <p class="comment-title">Zijn er nog opmerkingen?</p>
+
+                    @auth()
+
+                    <form action="/posts/{{$post->id}}/comment" method="post">
+
+                        @csrf
+
+                        <div class="form-group">
+
+                        <label for="txtComment">Opmerking</label>
+
+                        <textarea class="form-control" name="txtComment" id="txtComment" rows="5"></textarea>
+
+                        </div>
+
+                        <button type="submit" class="btn btn-primary full-width">Opmerking opslaan</button>
+
+                    </form>
+
+                    @else
+
+                        <div class="alert alert-danger" role="alert">
+
+                            <strong>Log in om een opmerking te maken</strong>
+
+                            <a id="btn-comment-login" class="btn btn-primary pull-right" href="/login" role="button">Inloggen</a>
+
+                        </div>
+
+                    @endauth
+
                 </div>
 
             </div>
 
         </div>
-
+        
     </div>
 
 @endsection
